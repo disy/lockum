2e39be1bd2261035f5b8eecb1a346ad7efcd45ff
Ahmet Eroglu <ahmet.eroglu@uni-konstanz.de>
2018-10-11 13:19:47 +0200

    deleted unneccesary folders

--- dist/DataConvertionCalculations.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class DataConvertionCalculations {
-    static byteArrayToBase64(byteArray: any): string;
-    static base64ToByteArray(base64String: any): Uint8Array;
-    static byteArrayToString(byteArray: any): string;
-    static stringToByteArray(s: any): Uint8Array;
-    static convertToHex(buffer: any): string;
-}
--- dist/Sender.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class Sender {
-    constructor();
-    /**
-     * @param locationInfo refers to location related elements. Consists of latitude, longitude and tolerance distance, respectively.
-     * @param message refers to plain text that will be encrypted
-     * @returns a promise that includes encrypted text, tolerance distance, key hash, salt and iv
-     */
-    encryptMessage(locationInfo: [number, number, number], message: string): PromiseLike<Promise<[string, string, Uint8Array, Uint8Array, number]>>;
-}
