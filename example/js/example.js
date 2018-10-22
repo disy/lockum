94463e92103c39e5da3f39fe29f75726dd3e937f
ahmet.eroglu@uni-konstanz.de <ahmet.eroglu@uni-konstanz.de>
2018-10-22 15:38:17 +0200

    new locations selection

--- example/js/example.js
+++ example/js/example.js
@@ -2,6 +2,8 @@
 var lockumLib = lockum
 
 $("#encryptButton").click(function () {
+    let latitude
+    let longitude
     let output = document.getElementById("out")
 
     if (!navigator.geolocation) {
@@ -9,13 +11,47 @@ $("#encryptButton").click(function () {
         return
     }
 
-    navigator.geolocation.getCurrentPosition(success, error, geo_options)
-    function success(position) {
+    let locationSelection = document.querySelector('input[name="location"]:checked').value
 
-        let selectedLocation = document.querySelector('input[name="location"]:checked').value
+    if (locationSelection == "Current") {
 
-        let latitude = position.coords.latitude
-        let longitude = position.coords.longitude
+        navigator.geolocation.getCurrentPosition(success, error, geo_options)
+        function success(position) {
+
+            let latitude = position.coords.latitude
+            let longitude = position.coords.longitude
+
+            //get plaintext and tolerance distance from the html page
+            let plaintext = $("#messageToEncrypt").val()
+            let toleranceDistance = parseInt(toleranceDistanceField.value.toString())
+
+            let locationData = [latitude, longitude, toleranceDistance]
+
+            //library call. It returns an object of  ciphertext,key hash, iv, salt and tolerance distance
+            let ciphertext = lockumLib.encrypt(locationData, plaintext)
+            ciphertext.then(function (ciphertextResult) {
+                //save values to the browser from library call result so that receiver can take them
+                const saltArray = Array.from(ciphertextResult.getSalt())
+                const storedSalt = JSON.stringify(saltArray)
+                const ivArray = Array.from(ciphertextResult.getIV())
+                const storedIV = JSON.stringify(ivArray)
+                localStorage.setItem("keyhash", ciphertextResult.getKeyHash())
+                localStorage.setItem("salt", storedSalt)
+                localStorage.setItem("iv", storedIV)
+                localStorage.setItem("ciphertext", ciphertextResult.getEncryptedText())
+                localStorage.setItem("toleranceDistance", ciphertextResult.getToleranceDistance())
+
+                //set the ciphertext and keyhash values in browser
+                $("#keyhashField").text(ciphertextResult.getKeyHash())
+                $("#messageToDecrypt").text(ciphertextResult.getEncryptedText())
+                $("#saltField").text(ciphertextResult.getSalt())
+                $("#ivField").text(ciphertextResult.getIV())
+            }).catch(err => {
+                console.log("library couldnt encrypt the message: " + err)
+            })
+        }
+
+    } else {
 
         if (selectedLocation == "autopark") {
             latitude = 47.689401
@@ -31,7 +67,6 @@ $("#encryptButton").click(function () {
             latitude = 47.698774
             longitude = 9.266201
         }
-
 
         //get plaintext and tolerance distance from the html page
         let plaintext = $("#messageToEncrypt").val()
@@ -75,7 +110,8 @@ $("#encryptButton").click(function () {
 })
 
 $("#decryptButton").click(function () {
-
+    let latitude
+    let longitude
     let output = document.getElementById("out")
 
     if (!navigator.geolocation) {
@@ -83,25 +119,56 @@ $("#decryptButton").click(function () {
         return
     }
 
-    navigator.geolocation.getCurrentPosition(success, error, geo_options)
-    function success(position) {
+    let receiverLocationSelection = document.querySelector('input[name="receiverLocation"]:checked').value
 
-        let receiverLocation = document.querySelector('input[name="receiverLocation"]:checked').value
+    if (receiverLocationSelection == "Current") {
 
-        let latitude = position.coords.latitude
-        let longitude = position.coords.longitude
+        navigator.geolocation.getCurrentPosition(success, error, geo_options)
+        function success(position) {
+            latitude = position.coords.latitude
+            longitude = position.coords.longitude
 
-        if (receiverLocation == "autopark") {
+            //get the salt value from return value of the library call
+            const salt = localStorage.getItem("salt")
+            const retrievedSaltArray = JSON.parse(salt)
+            const saltBytes = new Uint8Array(retrievedSaltArray)
+
+            //get the iv from return value of the library call
+            const iv = localStorage.getItem("iv")
+            const retrievedIvArray = JSON.parse(iv)
+            const ivBytes = new Uint8Array(retrievedIvArray)
+
+            //get the cipher text,key hash and tolerance distance from library call
+            let ciphertext = localStorage.getItem("ciphertext")
+            let keyhash = localStorage.getItem("keyhash")
+            let toleranceDistance = parseInt(JSON.parse(localStorage.getItem("toleranceDistance")))
+
+            let locationData = [latitude, longitude, toleranceDistance]
+            let decryptionElements = [saltBytes, ivBytes, ciphertext, keyhash]
+
+            //call library to decrypt, returns the plain text and calculated key hash
+            let plaintext = lockumLib.decrypt(locationData, decryptionElements)
+            plaintext.then(function (plaintextResult) {
+                $("#keyhashFieldReceiver").text(plaintextResult.getKeyHash())
+                $("#cipherTextArea").text(plaintextResult.getEncryptedText())
+            }).catch(err => {
+                $("#keyhashFieldReceiver").text("library couldnt decrypt the ciphertext: " + err)
+                $("#cipherTextArea").text("library couldnt decrypt the ciphertext: " + err)
+            })
+        }
+    } else {
+
+        if (receiverLocationSelection == "autopark") {
             latitude = 47.689401
             longitude = 9.186046
-        } else if (receiverLocation == "zahringerplatz") {
+        } else if (receiverLocationSelection == "zahringerplatz") {
             latitude = 47.672352
             longitude = 9.183634
-        } else if (receiverLocation == "bahnhof") {
+        } else if (receiverLocationSelection == "bahnhof") {
             latitude = 47.658895
             longitude = 9.176983
         }
-        else if (receiverLocation == "meersburg") {
+        else if (receiverLocationSelection == "meersburg") {
             latitude = 47.698774
             longitude = 9.266201
         }
@@ -133,6 +200,7 @@ $("#decryptButton").click(function () {
             $("#keyhashFieldReceiver").text("library couldnt decrypt the ciphertext: " + err)
             $("#cipherTextArea").text("library couldnt decrypt the ciphertext: " + err)
         })
+
     }
 
     function error() {
