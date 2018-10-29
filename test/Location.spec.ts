2adffc098f825c2e6118a71be8e9d2a712acfc53
sualko <klaus@jsxc.org>
2018-10-29 15:23:07 +0100

    fix location test

--- test/Location.spec.ts
+++ test/Location.spec.ts
@@ -9,7 +9,7 @@ describe('Location convertions', ()=>{
         let location = new Location(47.679970,9.163593,10)
         let keyInput  = location.getTransformedLocation()
         expect(keyInput[0]).to.equal(877925)
-        expect(keyInput[1]).to.equal(-16635)
+        expect(keyInput[1]).to.equal(-151635)
     });
 
     it('given locations of 47.679970,-9.163593 with tolerance distance of 10, results should be 877925 and  16635 ', ()=> {
@@ -17,7 +17,7 @@ describe('Location convertions', ()=>{
         let location = new Location(47.679970,-9.163593,10)
         let keyInput  = location.getTransformedLocation()
         expect(keyInput[0]).to.equal(877925)
-        expect(keyInput[1]).to.equal(16635)
+        expect(keyInput[1]).to.equal(151635)
     });
 
     it('given locations of -47.679970,9.163593 with tolerance distance of 10, results should be -877925 and -16635', ()=> {
@@ -25,16 +25,16 @@ describe('Location convertions', ()=>{
         let location = new Location(-47.679970,9.163593,10)
         let keyInput  = location.getTransformedLocation()
         expect(keyInput[0]).to.equal(-877925)
-        expect(keyInput[1]).to.equal(-16635)
+        expect(keyInput[1]).to.equal(-151635)
     });
 
     it('given locations of -47.679970,-9.163593 with tolerance distance of 10, results should be -877925 and 16635', ()=> {
         let location = new Location(-47.679970,-9.163593,10)
         let keyInput  = location.getTransformedLocation()
         expect(keyInput[0]).to.equal(-877925)
-        expect(keyInput[1]).to.equal(+16635)
+        expect(keyInput[1]).to.equal(+151635)
     });
 
-    
+
 });
 
